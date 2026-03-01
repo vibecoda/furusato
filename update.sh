e4462c862b4826fbd0ec12729403fe58c8962275
Shambhu Shrestha <shambhushrestha@gmail.com>
2026-03-01 18:41:15 +0900

    fix typo

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python scripts/fetch_municipalities.py
-python scripts/fetch_tokyo_shopts.py
+python scripts/fetch_tokyo_shops.py
 python scripts/fetch_hachipay_restaurants.py
 python scripts/geocode.py
